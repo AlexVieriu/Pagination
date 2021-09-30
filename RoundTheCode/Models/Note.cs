e6dc5bbcba30fe1eb8b29b825c5246cfc455fac2
Your Name <you@example.com>
2021-09-30 16:15:41 +0300

    Adding EventCallBack Examples

---
+++ RoundTheCode/Models/Note.cs
@@ -0,0 +1,14 @@
+﻿namespace RoundTheCode.Models;
+
+public class Note
+{
+    public string Message { get; set; }
+    public DateTimeOffset Created { get; set; }
+
+    public Note(string message)
+    {
+        Message = message;
+        Created = DateTimeOffset.Now;
+    }
+}
+
---
+++ RoundTheCode_Modified/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace RoundTheCode_Modified.Models;
+
+public class Item
+{
+    public string Text { get; set; }
+    public DateTime CDate { get; set; } = DateTime.Now;
+}
+
